f40dc6104b6539c6c836e3fff31e36f39ae47788
oli ullah <oli.jewel@northsouth.edu>
2023-11-19 11:32:24 +0600

    listening for load and hashchange events

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -1,5 +1,6 @@
 import icons from 'url:../img/icons.svg';
-
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -33,9 +34,14 @@ const renderSpinner = function (parentEl) {
 const showRecipe = async function () {
   try {
     //1. Loading recipe
+    const id = window.location.hash.slice(1);
+
+    console.log(id);
+
+    if (!id) return;
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -166,4 +172,8 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+// showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
